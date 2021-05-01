aab7955c806440577a41caba398f653e0ca423ef
ldog3211 <31681683+ldog3211@users.noreply.github.com>
2021-05-01 10:22:34 -0500

    Updated with Genesis Block info
    Updated Hash and Merkle

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -110,8 +110,8 @@ public:
 
         genesis = CreateGenesisBlock(1231006505, 2083236893, 0x1d00ffff, 1, 50 * COIN);
         consensus.hashGenesisBlock = genesis.GetHash();
-        assert(consensus.hashGenesisBlock == uint256S("0x000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f"));
-        assert(genesis.hashMerkleRoot == uint256S("0x4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b"));
+        assert(consensus.hashGenesisBlock == uint256S("0x0000000092af8aebd580df97f5a97d9a5febe354b3fab3313e1366668989062e"));
+        assert(genesis.hashMerkleRoot == uint256S("0xf8e686ac409c661ed0df1c5b6c4557b3f2717776dad5150655086e58b3ef6f91"));
 
         // Note that of those which support the service bits prefix, most only support a subset of
         // possible options.
